d47fe644f344c11d480465c03d8acc841fc7f30e
yuzhiyuan <1048921160@qq.com>
2019-10-23 10:28:07 +0800

    修改代码可以正常运行

--- test-dubbo-boot-consumer/src/main/java/yzy/testdubbobootconsumer/controller/DemoConsumerController.java
+++ test-dubbo-boot-consumer/src/main/java/yzy/testdubbobootconsumer/controller/DemoConsumerController.java
@@ -2,17 +2,24 @@ package yzy.testdubbobootconsumer.controller;
 
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.google.common.collect.Maps;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import yzy.testdubbobootapi.dubbo.DemoService;
 
+import java.util.Map;
+
 @RestController
 public class DemoConsumerController {
-    @Reference
+    // check参数代表是否检查服务提供者
+    @Reference(check = false)
     private DemoService demoService;
 
     @GetMapping("/dubbo/test")
-    public void test() {
-        demoService.sayHello("yuzhiyuan");
+    public Map<String, Object> test() {
+        String yuzhiyuan = demoService.sayHello("yuzhiyuan");
+        Map<String,Object> map = Maps.newHashMap();
+        map.put("error",yuzhiyuan);
+        return map;
     }
 }
--- test-dubbo-boot-provider/src/main/java/yzy/testdubbobootprovider/TestDubboBootProviderApplication.java
+++ test-dubbo-boot-provider/src/main/java/yzy/testdubbobootprovider/TestDubboBootProviderApplication.java
@@ -1,9 +1,11 @@
 package yzy.testdubbobootprovider;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class TestDubboBootProviderApplication {
 
     public static void main(String[] args) {
